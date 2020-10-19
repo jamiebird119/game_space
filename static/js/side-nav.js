038cd899574862861dfd8b80530164cb7dd67107
jamiebird119 <jamie.bird119@gmail.com>
2020-10-19 09:53:14 +0000

    Updated links to mobile nav and altered toast styling

--- static/js/side-nav.js
+++ static/js/side-nav.js
@@ -1,5 +1,5 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "150px";
+  document.getElementById("mySidenav").style.width = "170px";
 }
 
 /* Set the width of the side navigation to 0 */
